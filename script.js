6ef10cace23b506bac28eecef586caac14acbcc9
A <68278454+d8bu@users.noreply.github.com>
2021-11-03 15:39:50 +0500

    uploads

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const title = document.getElementsByTagName('title');
+title[0].innerHTML = Math.random().toString(16).substr(2, 8);
+
+function getInputValue() {
+    let inputVal = document.getElementById("inputId").value,
+        get = md5(inputVal),
+        div = document.getElementById('textId');
+
+    div.textContent = `${get}Q!`;
+}
+
+document.getElementById("copyBtn")
+        .onclick = function() {
+          let text = document.getElementById("textId").value;
+          navigator.clipboard.writeText(text)
+            .then(() => {
+            // Получилось!
+          });
+}
