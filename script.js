d4322a63fd88a74166dad7c2187c727b65ce9e16
Aleksandr <busiru@gmail.com>
2022-01-04 18:07:27 +0500

    substring

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function getInputValue() {
         get = md5(inputVal),
         div = document.getElementById('textId');
 
-    div.textContent = `${get}Q!`;
+    div.textContent = `${(get.substr(0, 14))}Q!`;
 }
 
 document.getElementById("copyBtn")
