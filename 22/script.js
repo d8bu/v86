4d493125d7cd84ccc0185169eb8a0e897bd5f989
Aleksandr <busiru@gmail.com>
2022-01-04 22:10:48 +0500

    folders

---
+++ 22/script.js
@@ -0,0 +1,18 @@
+const title = document.getElementsByTagName('title');
+title[0].innerHTML = Math.random().toString(16).substr(2, 8);
+
+function getInputValue() {
+    let inputVal = document.getElementById("inputId").value,
+        get = md5(inputVal),
+        div = document.getElementById('textId');
+
+    div.textContent = `${(get.substr(0, 20))}T$*`;
+}
+
+document.getElementById("copyBtn")
+        .onclick = function() {
+          let text = document.getElementById("textId").value;
+          navigator.clipboard.writeText(text)
+            .then(() => {
+          });
+}
